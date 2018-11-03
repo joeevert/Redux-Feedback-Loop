d886012a77e642b5c9bf1384be70652e548015fd
Joe Evert <joeevert@gmail.com>
2018-11-03 18:54:52 -0500

    prevent default added to comments click

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,7 +9,8 @@ class Comments extends Component {
   }
 
   // click handler for SUBMIT button - will proceed to complete (view #4)
-  handleClick = () => {
+  handleClick = (event) => {
+    event.preventDefault();
     console.log('in handleClick, comments');
     this.props.dispatch( {type: 'ADD_COMMENTS', payload: this.state} );
     this.props.history.push('/v5complete');
@@ -27,14 +28,14 @@ class Comments extends Component {
 
   // POST to db - called in handleClick
   sendFeedback = () => {
-    console.log('in sendFeedback');
+    console.log('in sendFeedback', this.props.reduxState.feedbackReducer);
     axios({
       method: 'POST',
       url: '/api/feedback',
       data: this.props.reduxState.feedbackReducer
     })
     .then((response) => {
-      console.log('sending feedback to db', response);
+      console.log('sending feedback to db', response.data);
     })
     .catch((error) => {
       console.log('error sending feedback', error);
