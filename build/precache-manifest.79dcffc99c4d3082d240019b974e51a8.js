d4123ceb770d0ff8a7f15c83cc997a9e8e3fea64
Joe Evert <joeevert@gmail.com>
2019-01-05 18:05:31 -0600

    build

---
+++ build/precache-manifest.79dcffc99c4d3082d240019b974e51a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb6c3714d56fd8958452",
+    "url": "/static/css/main.cfb72b23.chunk.css"
+  },
+  {
+    "revision": "bb6c3714d56fd8958452",
+    "url": "/static/js/main.bb6c3714.chunk.js"
+  },
+  {
+    "revision": "57e88df51db53b33d58f",
+    "url": "/static/js/1.57e88df5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d70c656cca6f34a0981ca0efaced77b5",
+    "url": "/index.html"
+  }
+];
